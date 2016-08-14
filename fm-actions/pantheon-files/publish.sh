fe056f01902b114d50d91beac4410c19fdec8fef
Sly_tom_cat <slytomcat@mail.ru>
2016-08-14 15:51:56 +0300

    Additional translations in actions for Dolphin and pantheon-files

--- fm-actions/pantheon-files/publish.sh
+++ fm-actions/pantheon-files/publish.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 #### Author: Fahreev Eldar ####
+
+### Language
+lang_home="/usr/share/yd-tools/translations"
+
+lang_file="$lang_home/actions-$(echo $LANG | cut -c 1-2).lang"
+
+if [ ! -f $lang_file ]; then
+    lang_file="$lang_home/actions-en.lang"
+fi
+
+source $lang_file
+
+
 f=$(yandex-disk publish "$1")
 out=$?
 if [ $out == 0 ]; then
     echo "$f" | xclip -filter -selection clipboard
-    notify-send Яндекс.Диск "Ссылка на файл: $1 скопирована в буфер обмена."
+    notify-send Yandex.Disk "$_Symlink $*
+
+$f
+
+$_Copy."
+
 else
-    notify-send Яндекс.Диск "$f"
+    notify-send Yandex.Disk "$f"
 fi
 
--- fm-actions/pantheon-files/unpublish.sh
+++ fm-actions/pantheon-files/unpublish.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #### Author: Fahreev Eldar ####
-f=$(yandex-disk unpublish "$1")
-out=$?
-if [ $out == 0 ]; then
-    notify-send Яндекс.Диск "Публичная ссылка на файл $f удалена"
-else
-    notify-send Яндекс.Диск "$f"
-fi
+
+notify-send Yandex.Disk $(yandex-disk unpublish "$1")
+
+#f=$(yandex-disk unpublish "$1")
+#out=$?
+#if [ $out == 0 ]; then
+#    notify-send Yandex.Disk "$f"
+#else
+#    notify-send Yandex.Disk "$f"
+#fi
 
 
