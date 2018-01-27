6c69bfc29e6644a732f0437025d3f8da99ff2f87
Sly_tom_cat <slytomcat@mail.ru>
2018-01-27 11:54:58 +0300

    prepare.sh update

--- build/prepare.sh
+++ build/prepare.sh
@@ -2,7 +2,12 @@
 
 # prepare changelog
 version=$(sed -n "/appVer = '.*'/p" ../yandex-disk-indicator.py | grep -o '[0-9]\.[0-9]\.[0-9]*')
+#series='artful'
+#series='bionic'
+#series='trusty'
 series='xenial'
+#series='zesty'
+
 clear
 
 sed "s/@series/$series/" changelog > yd-tools/debian/changelog
