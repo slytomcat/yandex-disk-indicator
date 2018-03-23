ec7720e7834ac335759b0fabd52ec52e2a25b90a
Sly_tom_cat <slytomcat@mail.ru>
2018-03-23 16:11:41 +0300

    1.10.2

--- yandex-disk-indicator.py
+++ yandex-disk-indicator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 appName = 'yandex-disk-indicator'
-appVer = '1.10.1'
+appVer = '1.10.2'
 #
 from datetime import datetime
 COPYRIGHT = 'Copyright ' + '\u00a9' + ' 2013-' + str(datetime.today().year) + ' Sly_tom_cat'
