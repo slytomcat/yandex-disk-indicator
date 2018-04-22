7898e06e45835d00ef3554e9daf909c98f44dfd6
Sly_tom_cat <slytomcat@mail.ru>
2018-04-22 12:20:37 +0300

    1.10.3 prepare

--- yandex-disk-indicator.py
+++ yandex-disk-indicator.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 appName = 'yandex-disk-indicator'
-appVer = '1.10.2'
+appVer = '1.10.3'
 #
 from datetime import datetime
 COPYRIGHT = 'Copyright ' + '\u00a9' + ' 2013-' + str(datetime.today().year) + ' Sly_tom_cat'
