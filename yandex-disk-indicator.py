442a32d0a13aa412236f87fd62ea1b597b1fc042
Sly_tom_cat <slytomcat@mail.ru>
2018-10-18 13:59:22 +0300

    more debugging

--- yandex-disk-indicator.py
+++ yandex-disk-indicator.py
@@ -463,7 +463,7 @@ class YDDaemon(object):         # Yandex.Disk daemon interface
     cmd = [self.__YDC, '-c', self.config.fileName, 'status']
     if not userLang:      # Change locale settings when it required
       cmd = ['env', '-i', 'LANG=''en_US.UTF8''', "TMPDIR=%s"%self.tmpDir] + cmd
-    #logger.debug('cmd = %s' % str(cmd))
+      logger.debug('status cmd = %s' % str(cmd))
     try:
       output = check_output(cmd, universal_newlines=True)
     except:
